0567569ed277e23a1e55219b9369192e0242bb5b
Mattias Kleman <mattias.kleman@gmail.com>
2021-10-06 11:50:10 +0200

    Add project files.

---
+++ Internetbanken/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Internetbanken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen till din internetbank ! ");
+            {
+               
+            }
+              
+            
+
+            string usersChoice = Console.ReadLine();
+            Console.WriteLine("Ange ditt användarnamn och din pinkod");
+
+            while (usersChoice)
+            {
+
+            }
+            }
+            
+
+        }
+    }
+
