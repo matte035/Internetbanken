33579ce9b93d3f2d162a40f9a9812c4152a026e3
Mattias Kleman <mattias.kleman@gmail.com>
2021-10-12 10:58:20 +0200

    Tillagt loopar

--- Internetbanken/Program.cs
+++ Internetbanken/Program.cs
@@ -7,32 +7,45 @@ namespace Internetbanken
         static void Main(string[] args)
         {
 
-                Console.WriteLine("Välkommen till din internetbank ! ");
-                Console.WriteLine("Vänligen ange ditt användarnamen och pinkod för att logga in");
-                
-                // Using a multi demisionell array to store my users and passwords
-                string[,] users = new string[5, 2];
-                users[0, 0] = "Tobias";
-                users[0, 1] = "1234";
+            Console.WriteLine("Välkommen till din internetbank ! ");
+            Console.WriteLine("Vänligen ange ditt användarnamen och pinkod för att logga in");
+            string username = Console.ReadLine();
 
-                users[1, 0] = "Anas";
-                users[1, 1] = "1337";
+            // Using a multi demisionell array to store my users and passwords
+            string[,] users = new string[5, 2];
+            
+            users[0, 0] = "Tobias";
+            users[0, 1] = "1234";
 
-                users[2, 0] = "Mattias";
-                users[2, 1] = "1739";
+            users[1, 0] = "Anas";
+            users[1, 1] = "1337";
 
-                users[3, 0] = "Kalle";
-                users[3, 1] = "1717";
+            users[2, 0] = "Mattias";
+            users[2, 1] = "1739";
 
-                users[4, 0] = "Philip";
-                users[4, 1] = "5678";
+            users[3, 0] = "Kalle";
+            users[3, 1] = "1717";
 
-            foreach (string user in users)
+            users[4, 0] = "Philip";
+            users[4, 1] = "5678";
+            
+
+            for (int i = 0; i < users.GetLength(0); i++)
             {
-                Console.WriteLine(user);
+               
+
+                if (users [i, 0] == username)
+                {
+                    Console.WriteLine("Du är inloggad");
+                }
+                else if(users [i, 0] != username)
+                {
+                    Console.WriteLine("Du har skrivit fel användarnamn");   
+                }
+                
             }
 
-          
+        }
     }
 }
 
