de242a1ddb8fe2da79c8dad675875957fd0f99ea
Mattias Kleman <mattias.kleman@gmail.com>
2021-12-09 18:25:50 +0100

    Tillagda funktioner

--- Internetbanken/Program.cs
+++ Internetbanken/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Internetbanken
 {
@@ -9,14 +10,16 @@ namespace Internetbanken
 
             Console.WriteLine("Välkommen till din internetbank ! ");
 
+            decimal Sparkonto = 12345.67M;
+            decimal Lönekonto = 100.11M ;
 
 
             // Using a multi demisionell array to store my users and passwords
             string[,] users = new string[5, 2];
-
+            
             users[0, 0] = "Tobias";
             users[0, 1] = "1234";
-
+            
             users[1, 0] = "Anas";
             users[1, 1] = "1337";
 
@@ -30,33 +33,23 @@ namespace Internetbanken
             users[4, 1] = "5678";
 
 
-            // If the user information is true or false.
-            //bool så börjar false(ej inloggad)
-
             bool checkLogIN = false;
+            
 
-            for (int x = 0; x < 3; x++)
+            for (int x = 0; x < 3; x++)// Ger tre inloggsförsök.
             {
-
-
                 Console.WriteLine("Vänligen ange ditt användarnamen");
-
                 string username = Console.ReadLine();
                 Console.WriteLine("Ange din pinkod");
                 string password = Console.ReadLine();
 
-
-
-                for (int i = 0; i < users.GetLength(0); i++)
+                for (int i = 0; i < users.GetLength(0); i++)// Går genom alla användare.
                 {
 
-                    if (users[i, 0] == username && users[i, 1] == password)
+                    if (users[i, 0] == username && users[i, 1] == password)// kollar om uppgifterna stämmer överrens.
                     {
                         checkLogIN = true;
-                        inloggMeny();
-
                     }
-
 
                 }
 
@@ -64,66 +57,63 @@ namespace Internetbanken
                 if (checkLogIN)
                 {
                     Console.WriteLine("Du är inloggad " + username);
-                    inloggMeny();
-                }
+                    
+                    while (checkLogIN == true)
+                    {
+                        Console.WriteLine("Du kan välja dessa alternativ \n 1- Se dina konton \n 2- Överföring mellan konton \n 3- Ta ut pengar \n 4 Logga ut");
 
+                        int userInputt = int.Parse(Console.ReadLine());
+                        bool W = Convert.ToBoolean(userInputt);
+
+
+                        switch (userInputt)
+                        {
+                            case 1:
+                                SeKonton();
+                                Console.WriteLine("Klicka enter för att komma till huvudmenyn");
+                                Console.WriteLine("Dessa är dina konton :  Sparkonto = {0} Kr  Lönekonto = {1} Kr ", Sparkonto, Lönekonto);
+
+                                break;
+
+                            case 2:
+                                Transfer();
+                                Console.WriteLine("Klicka enter för att komma till huvudmenyn");
+
+                                break;
+                            case 3:
+                                Uttag();
+                                Console.WriteLine("Klicka enter för att komma till huvudmenyn");
+
+                                break;
+                            case 4:
+
+                                UtLoggning();
+                                checkLogIN = false;
+                                break;
+                            default:
+                                Console.WriteLine("Du har valt ett ogiltigt val!\n Tryck enter för att fortsätta", userInputt);
+                                break;
+                        }
+
+
+                        Console.ReadKey();
+                        
+                    }
+                    
+                }
                 else
                 {
                     Console.WriteLine("Du har skrivit fel användarnamn");
                 }
 
             }
-           
 
-        }
-
-
-
-        static void inloggMeny()
-
-        {
-            Console.WriteLine("Du kan välja dessa alternativ \n 1- Se dina konton \n 2- Överföring mellan konton \n 3- Ta ut pengar \n 4 Logga ut");
-
-            int userInputt = int.Parse(Console.ReadLine());
-
-            
-
-            switch (userInputt)
-            {
-                case 1:
-                    SeKonton();
-                    Console.WriteLine("Klicka enter för att komma till huvudmenyn");
-                   
-                    break;
-                
-                case 2:
-                    Transfer();
-                    Console.WriteLine("Klicka enter för att komma till huvudmenyn");
-                    
-                    break;
-                case 3:
-                    Uttag();
-                    Console.WriteLine("Klicka enter för att komma till huvudmenyn");
-                    
-                    break;
-                case 4:
-                    UtLoggning();
-                    Console.WriteLine("Klicka enter för att komma till huvudmenyn");
-                    
-                    break;
-                    
-            }
-            Console.ReadKey();
-
-        }
-       
-        // While loop som användaren kan stanna kvar i menyn. Tills användaren väljer att "Logga Ut"
-        
-     
+        }// Här "stängs" programmen
 
         static void SeKonton()
         {
             Console.WriteLine("Välkommen till dina konton");
+            
         }
 
         static void Transfer()
@@ -139,6 +129,7 @@ namespace Internetbanken
         static void UtLoggning()
         {
             Console.WriteLine("Logga ut");
+            
             
         }
 
